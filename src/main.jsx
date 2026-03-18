f989b9e9c1e517d98a9b39281b3648d8a960767a
Bala-ramesh <balarmesh@gmail.com>
2026-03-18 23:15:31 +0000

    fix: add router basename and verify vite config

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/React_web">
       <MotionProvider>
         <App />
       </MotionProvider>
