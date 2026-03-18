bafc7857882dadaab475f1865840bfb8b7fafdfb
Bala-ramesh <balarmesh@gmail.com>
2026-03-18 11:26:45 +0000

    fix: correct base path format

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This matches your GitHub repository name
+  base: "https://github.com/Bala-ramesh/React_web.git",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
