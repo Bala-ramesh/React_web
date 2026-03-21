48f49d32e9fb50f10d96f50400ecb94cd048a21a
Bala-ramesh <balarmesh@gmail.com>
2026-03-21 10:23:34 +0000

    routing problem

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/React_web">
+    <BrowserRouter >
       <MotionProvider>
         <App />
       </MotionProvider>
